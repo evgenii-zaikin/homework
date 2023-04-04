9e03bc8c51dc2727a76a043f6a06aca1d6fdaa60
evgenii-zaikin <74616157+evgenii-zaikin@users.noreply.github.com>
2023-04-04 19:15:13 +0300

    Update 4.1.4.cpp

--- 4.1.4.cpp
+++ 4.1.4.cpp
@@ -1,1 +1,10 @@
-sdfff
+ostream& operator<<(ostream& stream, const map<string, int>& map)
+ {stream << '{';
+  char comma[3] = {'\0',' ', '\0'};
+  for (const auto& pair : map)
+  {stream << comma << '{' << pair.first << "," << pair.second<<'}';
+   comma[0] = ',';
+  }
+  stream << '}';
+  return stream;
+}
